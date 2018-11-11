07fced4c6e5cf315953a611f9d6eb69f728a8c9d
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2018-11-11 16:25:38 +0300

    Fix preloading impl. to support Monix Task

--- core/src/main/scala/com/olegpy/shironeko/internals/Preloading.scala
+++ core/src/main/scala/com/olegpy/shironeko/internals/Preloading.scala
@@ -1,10 +1,23 @@
 package com.olegpy.shironeko.internals
 
+import scala.concurrent.{ExecutionContext, Promise}
+
+import cats.effect.IO
 import com.olegpy.shironeko.StoreBase
-import cats.effect.implicits._
 
 
 trait Preloading[F[_]] { this: StoreBase[F] =>
-  protected def preload[A](fa: F[A]): F[A] =
-    fa.toIO.start.unsafeRunSync().join.to[F]
+  protected def preload[A](fa: F[A]): F[A] = {
+    val p = Promise[A]
+    F.runAsync(fa) {
+      case Left(ex) => IO(p.failure(ex))
+      case Right(value) => IO(p.success(value))
+    }.unsafeRunSync()
+    F.async(cb => p.future.onComplete { try_ =>
+      cb(try_.toEither)
+    }(new ExecutionContext {
+      def execute(runnable: Runnable): Unit = runnable.run()
+      def reportFailure(cause: Throwable): Unit = cause.printStackTrace()
+    }))
+  }
 }
